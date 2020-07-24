a722662367bcdeac458bc27b28e8b114cd11e01b
zhangjianfei <zhangjianfei@foxgoing.com>
2020-07-24 16:19:23 +0800

    greeter

---
+++ base/greeter.js
@@ -0,0 +1,5 @@
+function greeter(user) {
+    return 'hello ' + user;
+}
+var user = 123;
+console.log(greeter(user));
---
+++ base/greeter.ts
@@ -0,0 +1,6 @@
+function greeter(user: String) {
+    return 'hello ' + user
+}
+
+const user = 123
+console.log(greeter(user))
